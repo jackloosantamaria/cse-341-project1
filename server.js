c655806918e4b73fbd918409287ee488b0d2f233
jackloosantamaria <jackloosantamaria@hotmail.com>
2025-01-15 13:31:31 -0500

    adding post, put, and delete

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
-
 
 mongodb.initDb((err) =>{
     if (err) {
         console.log(err);
     }
     else {
-        
         app.listen(port, () => {console.log(`Data is listening and node Running on port ${port}`)});
     }
 });
